e294414e9fd705de892a35b2665fc7149855202a
Andrey_Bogoroditskiy <Andrey_Bogoroditskiy@epam.com>
2024-02-21 14:11:52 +0300

    [EPMGCIPCNT-19] - Component to deliver footer navigation for the website

---
+++ src/components/footer/footer.constants.ts
@@ -0,0 +1,37 @@
+import Envelope from "@/assets/envelope.svg?react";
+import Linkedin from "@/assets/linkedin.svg?react";
+import Tiktok from "@/assets/tiktok.svg?react";
+
+// TODO: add correct icons after navigation merge
+export const socialLinks = [
+  {
+    label: "send message",
+    link: "/",
+    icon: Envelope,
+  },
+  {
+    label: "linkedin",
+    link: "/",
+    icon: Linkedin,
+  },
+  {
+    label: "instagram",
+    link: "/",
+    icon: Tiktok,
+  },
+  {
+    label: "facebook",
+    link: "/",
+    icon: Tiktok,
+  },
+  {
+    label: "tiktok",
+    link: "/",
+    icon: Tiktok,
+  },
+  {
+    label: "youtube",
+    link: "/",
+    icon: Tiktok,
+  },
+];
