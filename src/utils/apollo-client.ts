6c5eeb93995808f2c9fba7be136792c3fcd1d1a0
Andrey_Bogoroditskiy <Andrey_Bogoroditskiy@epam.com>
2024-02-28 16:18:43 +0300

    - Fix part of the data

--- src/utils/apollo-client.ts
+++ src/utils/apollo-client.ts
@@ -1,9 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  cache: new InMemoryCache({
-    addTypename: false,
-  }),
+  cache: new InMemoryCache({}),
   //TODO: replace with env file
   uri: "https://cdkz-admin-staging-service-g7ihm2zefq-uc.a.run.app/graphql", // `${import.meta.env.VITE_REACT_APP_BACKEND_URL}/graphql`,
 });
