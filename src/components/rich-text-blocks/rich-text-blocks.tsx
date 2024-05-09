7ca0d865eed1438f2b7b7c991dce0d0ab4a62d7b
Andrey_Bogoroditskiy <Andrey_Bogoroditskiy@epam.com>
2024-05-09 14:38:41 +0300

    Added two columns component

---
+++ src/components/rich-text-blocks/rich-text-blocks.tsx
@@ -0,0 +1,85 @@
+import { PropsWithChildren } from "react";
+import { BlocksContent, BlocksRenderer } from "@strapi/blocks-react-renderer";
+import cc from "classcat";
+import { Link } from "react-router-dom";
+import { Title } from "@/components/title/title.tsx";
+import { Typography } from "@/components/typography/typography.tsx";
+import {
+  RichTextBlocksConfig,
+  RichTextModifiersConfig,
+} from "@/components/rich-text-blocks/rich-text-blocks.types.ts";
+import "./rich-text-blocks.scss";
+
+export interface RichTextBlocksProps {
+  content: BlocksContent;
+  className?: string;
+}
+
+const getTitleTag = (level?: 1 | 2 | 3 | 4 | 5 | 6) => {
+  switch (level) {
+    case 1:
+      return "h1";
+    case 2:
+      return "h2";
+    case 3:
+      return "h3";
+    case 4:
+      return "h4";
+    case 5:
+      return "h5";
+    case 6:
+      return "h5";
+    default: {
+      return "h3";
+    }
+  }
+};
+
+const blocks: RichTextBlocksConfig = {
+  paragraph: ({ children }: PropsWithChildren<unknown>) => (
+    <Typography>{children}</Typography>
+  ),
+  quote: props => <blockquote>{props.children}</blockquote>,
+  code: props => (
+    <pre>
+      <code>{props.plainText}</code>
+    </pre>
+  ),
+  heading: ({ level, children }) => (
+    <Title as={getTitleTag(level)} className="rich-text-blocks-title">
+      {children}
+    </Title>
+  ),
+  link: ({ url, children }) => (
+    <Link
+      to={url ?? ""}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="rich-text-link"
+    >
+      {children}
+    </Link>
+  ),
+  list: props => {
+    const ListTag = props.format === "ordered" ? "ol" : "ul";
+    return <ListTag>{props.children}</ListTag>;
+  },
+  "list-item": props => <li>{props.children}</li>,
+  image: props => (
+    <img src={props.image.url} alt={props.image.alternativeText ?? ""} />
+  ),
+};
+
+const modifiers: RichTextModifiersConfig = {
+  bold: props => <strong>{props.children}</strong>,
+  italic: props => <em>{props.children}</em>,
+  underline: props => <u>{props.children}</u>,
+  strikethrough: props => <del>{props.children}</del>,
+  code: props => <code>{props.children}</code>,
+};
+
+export const RichTextBlocks = ({ content, className }: RichTextBlocksProps) => (
+  <div className={cc(["rich-text-blocks", className])}>
+    <BlocksRenderer content={content} blocks={blocks} modifiers={modifiers} />
+  </div>
+);
---
+++ src/features/two-columns-section/two-columns-section.tsx
@@ -0,0 +1,134 @@
+import { BlocksContent } from "@strapi/blocks-react-renderer";
+import cc from "classcat";
+import { ActionableArticle } from "@/features/two-columns-section/actionable-article/actionable-article.tsx";
+import { DataGraph } from "@/components/data-graph/data-graph.tsx";
+import { RichTextBlocks } from "@/components/rich-text-blocks/rich-text-blocks.tsx";
+import { RichTextMarkdown } from "@/components/rich-text-markdown/rich-text-markdown.tsx";
+import { SectionBase } from "@/components/section-base/section-base.tsx";
+import banner from "@/assets/images/big-height.jpg";
+import "./two-columns-section.scss";
+
+type ColumnContent =
+  | {
+      type: "article";
+      body?: string;
+      title?: string;
+    }
+  | {
+      type: "markdown";
+      body?: string;
+    }
+  | {
+      type: "rich-text";
+      body?: BlocksContent;
+    }
+  | {
+      type: "image";
+    }
+  | {
+      type: "video";
+    }
+  | {
+      type: "graph";
+    };
+
+const renderColumn = (item: ColumnContent) => {
+  switch (item.type) {
+    case "article": {
+      return <ActionableArticle body={item?.body} title={item?.title} />;
+    }
+    case "markdown": {
+      return <RichTextMarkdown>{item?.body}</RichTextMarkdown>;
+    }
+    case "rich-text": {
+      return <RichTextBlocks content={item?.body ?? []} />;
+    }
+    case "image": {
+      return <img className="two-columns-section-image" src={banner} alt="" />;
+    }
+    case "video": {
+      return (
+        <video
+          controls
+          className="two-columns-section-video"
+          aria-label="video"
+        >
+          <source />
+        </video>
+      );
+    }
+    case "graph": {
+      return <DataGraph />;
+    }
+    default: {
+      return null;
+    }
+  }
+};
+
+export const TwoColumnsSection = () => {
+  const columns: ColumnContent[][] = [
+    [
+      {
+        type: "article",
+        title: "Our values",
+        body: "## Our values\n\nOur course is built upon the principles of equity, accessibility, inclusivity, and excellence. Available in both Kazakh and Russian languages, it caters to a wide range of schoolchildren, particularly those from rural areas, bridging the educational quality gap between urban and rural regions. Delivered online and free of charge for convenience and accessibility, we believe that every child should have the opportunity to gain digital literacy skills, regardless of their background, language, or economic status. We also promote engagement and participation, and upon course completion, electronic certificates serve as recognition of dedication to digital literacy improvement. Our commitment is to create an inclusive and digitally empowered society.\n",
+        // body: "Our course is built upon the principles of equity, accessibility, inclusivity, and excellence. Available in both Kazakh and Russian languages, it caters to a wide range of schoolchildren, particularly those from rural areas, bridging the educational quality gap between urban and rural regions. Delivered online and free of charge for convenience and accessibility, we believe that every child should have the opportunity to gain digital literacy skills, regardless of their background, language, or economic status. We also promote engagement and participation, and upon course completion, electronic certificates serve as recognition of dedication to digital literacy improvement. Our commitment is to create an inclusive and digitally empowered society."
+      } /*{
+    type: "article",
+    title: "Who is the course for",
+    body: "The Digital Literacy for kids and youth course is open to all middle school children aged 11-15 (grades 5 to 9). This age range was selected in response to the distance learning challenges these children faced during the COVID-19 outbreak and the findings from the ICILS 2018 report on digital literacy among 8th graders in Kazakhstan. It revealed that 50% of 8th graders are at Level 1, 27% at Level 2, and 25% at Level 3 of digital literacy. None of them achieved Level 4. This underscores the importance of educating this specific age group in digital literacy. By offering this online course, we aim to empower young individuals with the skills they need to thrive in an increasingly digital world. Our course ensures that children from diverse backgrounds can participate, bridging the digital divide and opening more opportunities."
+  }*/,
+      { type: "graph" },
+    ],
+    [
+      { type: "image" },
+      {
+        type: "rich-text",
+        body: [
+          {
+            type: "paragraph",
+            children: [{ type: "text", text: "A simple paragraph" }],
+          },
+        ],
+      },
+      /*{
+        type: "markdown",
+        title: "Our values",
+        body: "## Our values\n\n**Our** course is built upon ~~the~~ principles of equity, accessibility, inclusivity, and excellence. Available in both <u>Kazakh</u> and _Russian_ languages, it caters to a wide range of schoolchildren, particularly those from rural areas, bridging the educational quality gap between urban and rural regions. Delivered online and free of charge for convenience and accessibility, we believe that every child should have the opportunity to gain digital literacy skills, regardless of their background, language, or economic status. We also promote engagement and participation, and upon course completion, electronic certificates serve as recognition of dedication to digital literacy improvement. Our commitment is to create an inclusive and digitally empowered society.\n- first\n- second\n\n1.  First\n2.  Second\n\n\n```\nCode\n```\n\n\n>Quote",
+        // body: "Our course is built upon the principles of equity, accessibility, inclusivity, and excellence. Available in both Kazakh and Russian languages, it caters to a wide range of schoolchildren, particularly those from rural areas, bridging the educational quality gap between urban and rural regions. Delivered online and free of charge for convenience and accessibility, we believe that every child should have the opportunity to gain digital literacy skills, regardless of their background, language, or economic status. We also promote engagement and participation, and upon course completion, electronic certificates serve as recognition of dedication to digital literacy improvement. Our commitment is to create an inclusive and digitally empowered society."
+      }*/ /*{
+    type: "article",
+    title: "Who is the course for",
+    body: "The Digital Literacy for kids and youth course is open to all middle school children aged 11-15 (grades 5 to 9). This age range was selected in response to the distance learning challenges these children faced during the COVID-19 outbreak and the findings from the ICILS 2018 report on digital literacy among 8th graders in Kazakhstan. It revealed that 50% of 8th graders are at Level 1, 27% at Level 2, and 25% at Level 3 of digital literacy. None of them achieved Level 4. This underscores the importance of educating this specific age group in digital literacy. By offering this online course, we aim to empower young individuals with the skills they need to thrive in an increasingly digital world. Our course ensures that children from diverse backgrounds can participate, bridging the digital divide and opening more opportunities."
+  }*/
+    ],
+  ];
+
+  return (
+    <SectionBase className="two-columns-section">
+      <div className="two-columns-section-content">
+        {columns.map((row, index) => {
+          return (
+            <div
+              /* eslint-disable-next-line react/no-array-index-key */
+              key={index}
+              className={cc([
+                "two-columns-section-row",
+                // "two-columns-section-row-large-right", 1:2
+                // "two-columns-section-row-large-left" 2:1
+              ])}
+            >
+              {row.map((item, columnIndex) => (
+                /* eslint-disable-next-line react/no-array-index-key */
+                <div className="two-columns-section-column" key={columnIndex}>
+                  {renderColumn(item)}
+                </div>
+              ))}
+            </div>
+          );
+        })}
+      </div>
+    </SectionBase>
+  );
+};
