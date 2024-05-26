c020d053bed5bc3cdca1ce2863d36e8cdab01f74
Andrey_Bogoroditskiy <Andrey_Bogoroditskiy@epam.com>
2024-05-26 22:39:36 +0300

    Fix build

--- src/app/[locale]/[slug]/not-found.tsx
+++ src/app/[locale]/[slug]/not-found.tsx
@@ -1,8 +1,20 @@
-"use client";
+import { Title } from "@/components/title/title.tsx";
+import { Typography } from "@/components/typography/typography.tsx";
+import "./not-found.scss";
 
-import Error from "next/error";
+export interface NotFoundProps {
+  title: string;
+  description: string;
+}
+
+const NotFound = ({ title, description }: NotFoundProps) => (
+  <section className="not-found">
+    <Title as="h1" className="not-found-title">
+      {title}
+    </Title>
+    <Typography className="not-found-description">{description}</Typography>
+  </section>
+);
 
 // eslint-disable-next-line import/no-default-export,react/function-component-definition
-export default function NotFound() {
-  return <Error statusCode={404} />;
-}
+export default NotFound;
